a5704052643f85d34cf91f834896b9e3d15e8d71
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-04-24 08:53:50 +0300

    handleLoadProjects ipc handler added

--- src/services/models/Api.ts
+++ src/services/models/Api.ts
@@ -1,4 +1,7 @@
+import { Project } from "models";
+
 export interface Api {
 	init: () => Promise<string>;
+	loadProjects: () => Promise<Project[]>;
 	toWayTest: (payload: string) => Promise<string>;
 }
