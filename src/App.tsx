9470559ed59fbdf30f3c393a2466db1792fe6fad
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-04-29 22:19:40 +0300

    Render index page after app initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { ProjectsPage } from "pages/ProjectsPage";
 import { initAsync } from "services";
 
 function App() {
+  const [isInit, setIsInit] = useState(false);
+
   const init = async () => {
     const initMessage = await initAsync();
     console.log(initMessage);
+    setIsInit(true);
   };
 
   useEffect(() => {
     init();
   }, []);
 
-  return <ProjectsPage />;
+  return isInit ? <ProjectsPage /> : null;
 }
 
 export default App;
